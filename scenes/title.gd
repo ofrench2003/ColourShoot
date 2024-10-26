56b18cec90663fe7eb83f90cc52b33e8dee98ed3
Oliver French <oliverpfrench2003@gmail.com>
2024-10-26 22:38:36 +0100

    Title Screen
    Added a title screen with the ability to quit and play game. Last to setup is controls.

---
+++ scenes/title.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$fader.play("fadeIn")
+	await get_tree().create_timer(0.5).timeout
+	$Control/fadeColour.hide()
+
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+	if Input.is_action_just_pressed("fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	$Control/fadeColour.show()
+	$fader.play("fadeOut")
+	await get_tree().create_timer(0.5).timeout
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
